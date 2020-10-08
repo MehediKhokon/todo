c294b8792ba3a9270c768c5e589ef831d5b67d2e
MehediKhokon <mahedikhokon1@gmail.com>
2020-10-08 18:25:52 +0600

    Updates

---
+++ precache-manifest.a62ffdd95485020cf0f3ca95c12c3c8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e7dea1eb025de50425152281c7948e4",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "ed06e2d7d594bbaa6f9f",
+    "url": "/todo/static/css/main.862013d7.chunk.css"
+  },
+  {
+    "revision": "017898c5a0217a142f67",
+    "url": "/todo/static/js/2.8a39eb03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.8a39eb03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed06e2d7d594bbaa6f9f",
+    "url": "/todo/static/js/main.97681300.chunk.js"
+  },
+  {
+    "revision": "73306ce994a6ed0954a2",
+    "url": "/todo/static/js/runtime-main.a940b003.js"
+  }
+]);
